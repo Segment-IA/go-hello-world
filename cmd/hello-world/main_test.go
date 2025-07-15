d6b2099ac9bfced6f21c3e43885488cfaa59924b
Fernando Pessoa <fernando.pessoa@segment.net.br>
2025-07-15 16:40:49 -0300

    Add .dockerignore and .gitignore files; format main.go and main_test.go

--- cmd/hello-world/main_test.go
+++ cmd/hello-world/main_test.go
@@ -1,34 +1,35 @@
 package main
 
 import (
-    "testing"
-    "github.com/stretchr/testify/suite"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
 )
 
 type HelloWorldTestSuite struct {
-    suite.Suite
-    setupMessage string
+	suite.Suite
+	setupMessage string
 }
 
 func (suite *HelloWorldTestSuite) SetupSuite() {
-    suite.setupMessage = "Hello, World!"
+	suite.setupMessage = "Hello, World!"
 }
 
 func (suite *HelloWorldTestSuite) TearDownSuite() {
-    suite.setupMessage = ""
+	suite.setupMessage = ""
 }
 
 func (suite *HelloWorldTestSuite) TestHelloWorld() {
-    // expected := "Hello, World!"
-    actual := getHelloWorld()
-    // suite.Equal(expected, actual)
-    suite.Equal(actual, suite.setupMessage)
+	// expected := "Hello, World!"
+	actual := getHelloWorld()
+	// suite.Equal(expected, actual)
+	suite.Equal(actual, suite.setupMessage)
 }
 
 func getHelloWorld() string {
-    return "Hello, World!"
+	return "Hello, World!"
 }
 
 func TestHelloWorldTestSuite(t *testing.T) {
-    suite.Run(t, new(HelloWorldTestSuite))
+	suite.Run(t, new(HelloWorldTestSuite))
 }
